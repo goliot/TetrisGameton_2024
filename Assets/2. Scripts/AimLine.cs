3d414294df32d5f36cebad3a648ebf8e586cd289
goliot <cktnals2000@naver.com>
2024-11-06 13:29:59 +0900

    조준선, 조준점 생성

---
+++ Assets/2. Scripts/AimLine.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class AimRaycast : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    [SerializeField]
+    private GameObject crosshair;      // 조준점 인스턴스
+
+    public float maxDistance = 10f;
+    public LayerMask obstacleLayer;
+    public Transform gunTip;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, gunTip.position.z - Camera.main.transform.position.z));
+        Vector2 aimDirection = (mouseWorldPosition - gunTip.position).normalized;
+        float distanceToMouse = Vector2.Distance(gunTip.position, mouseWorldPosition);
+
+        RaycastHit2D hit = Physics2D.Raycast(gunTip.position, aimDirection, Mathf.Min(maxDistance, distanceToMouse), obstacleLayer);
+        Vector3 endPoint = hit.collider != null ? (Vector3)hit.point : mouseWorldPosition;
+
+        // 라인 렌더러에 시작 및 끝 위치 설정
+        lineRenderer.SetPosition(0, gunTip.position);
+        lineRenderer.SetPosition(1, endPoint);
+
+        // 조준점 위치 설정
+        if (crosshair != null)
+        {
+            crosshair.transform.position = mouseWorldPosition;
+        }
+    }
+}
